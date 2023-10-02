087b1bbf2a19bc9677a113d89cdad9657f80a751
taehun.kim <taehun.kim@motional.com>
2023-10-02 14:42:57 +0900

    move ml into its own file

---
+++ crowdstop/ml/multiple_object_tracker.py
@@ -0,0 +1,84 @@
+import os
+from typing import Any, Iterable
+import cv2
+import numpy as np
+from tqdm import tqdm
+
+from motrackers import CentroidTracker, CentroidKF_Tracker, SORT, IOUTracker
+from motrackers.utils.misc import draw_tracks
+
+from crowdstop.models.sompt import ImageAnnotation, SomptScene
+from crowdstop.models.enums import TrackerType, DetectorType
+
+
+class MultipleObjectTracker:
+
+    def __init__(self, tracker_type: TrackerType, detector_type: DetectorType) -> None:
+        
+        # set the tracker
+        if tracker_type is TrackerType.CentroidTracker:
+            self._tracker = CentroidTracker(max_lost=0, tracker_output_format='mot_challenge')
+        elif tracker_type is TrackerType.CentroidKF_Tracker:
+            self._tracker = CentroidKF_Tracker(max_lost=0, tracker_output_format='mot_challenge')
+        elif tracker_type is TrackerType.SORT:
+            self._tracker = SORT(max_lost=3, tracker_output_format='mot_challenge', iou_threshold=0.3)
+        elif tracker_type is TrackerType.IOUTracker:
+            self._tracker = IOUTracker(max_lost=2, iou_threshold=0.5, min_detection_confidence=0.4, 
+                max_detection_confidence=0.7, tracker_output_format='mot_challenge')
+        else:
+            raise ValueError('Unsupported tracker type!')
+
+        detector_configs = self._get_detector_configs(detector_type)
+        # construct the model
+        self._model = detector_type.detector_class(
+            confidence_threshold=0.5,
+            nms_threshold=0.2,
+            draw_bboxes=True,
+            use_gpu=False,
+            **detector_configs
+        )
+    
+    @classmethod
+    def _get_detector_configs(cls, detector_type: DetectorType) -> dict:
+        '''
+        Read environment variables to get filepaths of necessary configs
+        '''
+        args = dict()
+        for config_name in ['weights_path', 'configfile_path', 'labels_path']:
+            env_key = f'{detector_type.value}_{config_name}'.upper()
+            config_value = os.getenv(env_key)
+            assert config_value, f'Could not find necessary env var {env_key}'
+            
+            args[config_name] = config_value
+        return args
+    
+    def track(self, scene: SomptScene, show_output: bool = False) -> Iterable[tuple[np.ndarray, list[ImageAnnotation]]]:
+        for image in tqdm(scene.frames, total=len(scene)):
+
+            image = cv2.resize(image.cv2_image(), (700, 500))
+
+            bboxes, confidences, class_ids = self._model.detect(image)
+            tracks = self._tracker.update(bboxes, confidences, class_ids)
+            annotations: list[ImageAnnotation] = list()
+            for track in tracks:
+                frame, id, xmin, ymin, width, height, *_ = track
+                annotations.append(ImageAnnotation(
+                    frame=frame,
+                    person_id=id,
+                    x=xmin,
+                    y=ymin,
+                    width=width,
+                    height=height
+                ))
+                
+            updated_image = None
+            if show_output:
+                updated_image = self._model.draw_bboxes(image.copy(), bboxes, confidences, class_ids)
+                updated_image: np.ndarray = draw_tracks(updated_image, tracks)
+                
+            yield updated_image, annotations
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+                
+        cv2.destroyAllWindows()
--- crowdstop/motracker.py
+++ crowdstop/motracker.py
@@ -1,17 +1,15 @@
+import os
 import cv2 as cv
 from typer import Typer, Argument, Option
 from pathlib import Path
 from typing import Annotated, Iterable
-from enum import Enum
 import numpy as np
 from PIL import Image
-from tqdm import tqdm
+import itertools
 
-from motrackers.detectors import YOLOv3, Caffe_SSDMobileNet, TF_SSDMobileNetV2
-from motrackers import CentroidTracker, CentroidKF_Tracker, SORT, IOUTracker
-from motrackers.utils import draw_tracks
-
+from crowdstop.ml.multiple_object_tracker import MultipleObjectTracker
 from crowdstop.models.sompt import SomptScene
+from crowdstop.models.enums import DetectorType, TrackerType
 from crowdstop.moteval import calculate_motmetrics, metrics_motchallenge_files, compute_motchallenge
 
 
@@ -19,37 +17,21 @@ app = Typer(
     help='Object detections in input video using YOLOv3 trained on COCO dataset.'
 )
 
-
-class TrackerType(Enum):
-    CentroidTracker = 'CentroidTracker'
-    CentroidKF_Tracker = 'CentroidKF_Tracker'
-    SORT = 'SORT'
-    IOUTracker = 'IOUTracker'
-
-class DetectorType(Enum):
-    YOLOv3 = 'YOLOv3'
-    Caffe_SSDMobileNet = 'Caffe_SSDMobileNet'
-    # TF_SSDMobileNetV2 seems to draw a bounding box for "person" all around the image
-    TF_SSDMobileNetV2 = 'TF_SSDMobileNetV2'
-
 # dictionary to map detector types to their appropriate paths
 detector_models = {
     DetectorType.YOLOv3: {
-        'model_class': YOLOv3,
         'weights_path': '../../multi-object-tracker/examples/pretrained_models/yolo_weights/yolov3.weights',
-        'config_path': '../../multi-object-tracker/examples/pretrained_models/yolo_weights/yolov3.cfg',
+        'configfile_path': '../../multi-object-tracker/examples/pretrained_models/yolo_weights/yolov3.cfg',
         'labels_path': '../../multi-object-tracker/examples/pretrained_models/yolo_weights/coco_names.json',
     },
     DetectorType.Caffe_SSDMobileNet: {
-        'model_class': Caffe_SSDMobileNet,
         'weights_path': '../../multi-object-tracker/examples/pretrained_models/caffemodel_weights/MobileNetSSD_deploy.caffemodel',
-        'config_path': '../../multi-object-tracker/examples/pretrained_models/caffemodel_weights/MobileNetSSD_deploy.prototxt',
+        'configfile_path': '../../multi-object-tracker/examples/pretrained_models/caffemodel_weights/MobileNetSSD_deploy.prototxt',
         'labels_path': '../../multi-object-tracker/examples/pretrained_models/caffemodel_weights/ssd_mobilenet_caffe_names.json',
     },
     DetectorType.TF_SSDMobileNetV2: {
-        'model_class': TF_SSDMobileNetV2,
         'weights_path': '../../multi-object-tracker/examples/pretrained_models/tensorflow_weights/ssd_mobilenet_v2_coco_2018_03_29/frozen_inference_graph.pb',
-        'config_path': '../../multi-object-tracker/examples/pretrained_models/tensorflow_weights/ssd_mobilenet_v2_coco_2018_03_29.pbtxt',
+        'configfile_path': '../../multi-object-tracker/examples/pretrained_models/tensorflow_weights/ssd_mobilenet_v2_coco_2018_03_29.pbtxt',
         'labels_path': '../../multi-object-tracker/examples/pretrained_models/tensorflow_weights/ssd_mobilenet_v2_coco_names.json',
     },
 }
@@ -66,80 +48,44 @@ def main(
     limit: int = -1
 ) -> None:
 
-    # set the tracker
-    if tracker is TrackerType.CentroidTracker:
-        tracker = CentroidTracker(max_lost=0, tracker_output_format='mot_challenge')
-    elif tracker is TrackerType.CentroidKF_Tracker:
-        tracker = CentroidKF_Tracker(max_lost=0, tracker_output_format='mot_challenge')
-    elif tracker is TrackerType.SORT:
-        tracker = SORT(max_lost=3, tracker_output_format='mot_challenge', iou_threshold=0.3)
-    elif tracker is TrackerType.IOUTracker:
-        tracker = IOUTracker(max_lost=2, iou_threshold=0.5, min_detection_confidence=0.4, max_detection_confidence=0.7,
-                             tracker_output_format='mot_challenge')
+    assert not (output_gif and show_gif), 'Model can only output to screen or a GIF file, not both'
 
-    # get the selected detector model and relevant paths
-    detector = detector_models.get(detector_type)
-    if detector is None:
-        raise ValueError("Unsupported detector type")
+    # Populate env vars needed for detector configs. In production this will be done in Dockerfile
+    detector_config = detector_models.get(detector_type)
+    assert detector_config, f'Invalid detector config: {detector_type}'
+    for k, v in detector_config.items():
+        os.environ[f'{detector_type.value}_{k}'.upper()] = v
 
-    model_class = detector['model_class']
-    weights_path = detector['weights_path']
-    config_path = detector['config_path']
-    labels_path = detector['labels_path']
-
-    # construct the model
-    model = model_class(
-        weights_path=weights_path,
-        configfile_path=config_path,
-        labels_path=labels_path,
-        confidence_threshold=0.5,
-        nms_threshold=0.2,
-        draw_bboxes=True,
-        use_gpu=gpu
-    )
-
+    # Init multiple object tracker model and dataset scene to analyze
+    model = MultipleObjectTracker(tracker, detector_type)
     scene = SomptScene(dataset_dir, scene_num)
-
-    #### README ####
-    # these two output the same metrics but keeping both in case we want to bulk run using #1
-    # approach 1 does require reformatting of det.txt files to remove the space between commas
-
-    # APPROACH 1: print metrics using MOTMETRICS
-    metrics = metrics_motchallenge_files(data_dir='../sompt22/train')
-    print(metrics)
-    # APPROACH 2: print metrics using MOTRACKERS
-    gtSource = scene.annotation_fp
-    detSource = scene.detect_fp
-    metrics = calculate_motmetrics(gtSource, detSource)
-    print(metrics)
-
-    images = list(track(scene, model, tracker, show_gif))
+    
+    # Start lazy eval
+    tracks = model.track(scene, show_output=show_gif or output_gif)
     if limit != -1:
-        images = images[:limit]
+        # Only evaluate first n images
+        tracks = itertools.islice(tracks, limit)
+    
+    if show_gif:
+        for image, annotation in tracks:
+            cv.imshow("image", image)
     
     if output_gif:
-        save_as_gif(images, output_gif)
+        save_as_gif([image for image, _ in tracks], output_gif)
+        
+     # #### README ####
+    # # these two output the same metrics but keeping both in case we want to bulk run using #1
+    # # approach 1 does require reformatting of det.txt files to remove the space between commas
 
+    # # APPROACH 1: print metrics using MOTMETRICS
+    # metrics = metrics_motchallenge_files(data_dir='../sompt22/train')
+    # print(metrics)
+    # # APPROACH 2: print metrics using MOTRACKERS
+    # gtSource = scene.annotation_fp
+    # detSource = scene.detect_fp
+    # metrics = calculate_motmetrics(gtSource, detSource)
+    # print(metrics)
 
-def track(scene: SomptScene, model, tracker, show_gif: bool) -> Iterable[np.ndarray]:
-
-    for image in tqdm(scene.frames, total=len(scene)):
-
-        image = cv.resize(image.cv2_image(), (700, 500))
-
-        bboxes, confidences, class_ids = model.detect(image)
-        tracks = tracker.update(bboxes, confidences, class_ids)
-        updated_image = model.draw_bboxes(image.copy(), bboxes, confidences, class_ids)
-
-        updated_image: np.ndarray = draw_tracks(updated_image, tracks)
-
-        if show_gif:
-            cv.imshow("image", updated_image)
-        yield updated_image
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    cv.destroyAllWindows()
 
 def save_as_gif(image_arrays: Iterable[np.ndarray], output_fp: Path):
     
